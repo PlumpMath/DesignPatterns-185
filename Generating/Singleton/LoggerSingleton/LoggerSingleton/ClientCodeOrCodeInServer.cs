6332cde2ed44c06ac7c568726e0fade98d18b256
SergiyRomanchuk <poman4yk@gmail.com>
2016-02-23 18:55:19 +0200

    Create Singleton pattern solution

---
+++ Generating/Singleton/LoggerSingleton/LoggerSingleton/ClientCodeOrCodeInServer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LoggerSingleton
+{
+	class ClientCodeOrCodeInServer
+	{
+		static void Main(string[] args)
+		{
+			var client = new ClientCodeOrCodeInServer();
+			client.DoSomething();
+			// Delay
+			Console.ReadKey();
+		}
+		/// <summary>
+		/// This may be method in server logic and this can log some action
+		/// </summary>
+		public void DoSomething()
+		{
+			Logger.Instance.WriteMessage("I'm start...");
+			// Param
+			int n = 23;
+			for (int i = 0; i < n; i++)
+			{
+				if (i % 3 == 0)
+				{
+					Logger.Instance.WriteMessage(string.Format("My work in {0} from {1}", i, n));
+				}
+			}
+			Logger.Instance.WriteMessage("I'm END!");
+		}
+
+	}
+}
---
+++ Generating/Singleton/LoggerSingleton/LoggerSingleton/Logger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LoggerSingleton
+{
+	/// <summary>
+	/// This must be one instance in programm
+	/// And this must be thread-safe
+	/// </summary>
+	class Logger
+	{
+		private static Logger _instance;
+		// Lock object
+		private static object _locker = new object();
+		private static int _messageNumber;
+		private Logger()
+		{
+		}
+		// This can be property or method
+		// Access to logger
+		public static Logger Instance
+		{
+			get
+			{
+				// Thread-safe
+				// Only one thread may be set this
+				lock (_locker)
+				{
+					if (_instance == null)
+					{
+						_instance = new Logger();
+						_messageNumber = 0;
+					}
+				}
+				return _instance;
+			}
+		}
+
+		public void WriteMessage(string text)
+		{
+			_messageNumber++;
+			// Some logger logic
+			// Write in file, numerate strings, else
+			Console.WriteLine("{0}. {1}", _messageNumber, text);
+		}
+	}
+}
